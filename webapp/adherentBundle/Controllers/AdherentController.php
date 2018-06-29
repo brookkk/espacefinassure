61cbe3fdc298c3eb0f8d587cbfe1fd58e8b17a5e
brookkk <grini.abdellah@gmail.com>
2018-06-29 02:49:25 +0100

    ajout adherent controller

--- webapp/adherentBundle/Controllers/AdherentController.php
+++ webapp/adherentBundle/Controllers/AdherentController.php
@@ -5,6 +5,15 @@ include '../Classes/Crud_adherent.php';
 
 
 $no_licence = $_POST['no_licence'];
+$type_d_identite = $_POST['type_d_identite'];
+$id_identite = $_POST['id_identite'];
+$nom = $_POST['nom'];
+$prenom = $_POST['prenom'];
+$no_licence = $_POST['no_licence'];
+$no_licence = $_POST['no_licence'];
+$no_licence = $_POST['no_licence'];
+$no_licence = $_POST['no_licence'];
+$no_licence = $_POST['no_licence'];
 
 //$no_licence = "hoho";
 
