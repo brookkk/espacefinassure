e837882f11a544b92f9483cea96e6a0a5215eeb7
brookkk <grini.abdellah@gmail.com>
2018-06-29 02:43:43 +0100

    ajout adherent form01

--- webapp/ajouter_adherent.php
+++ webapp/ajouter_adherent.php
@@ -112,11 +112,59 @@ if(!$_SESSION['pseudo'])
     </div>
   </div>
 
+            <div class="form-group row">
+    <label for="nom" class="col-sm-2 col-form-label">Nom</label>
+    <div class="col-sm-4">
+      <input type="text" class="form-control" name="nom" placeholder="Nom">
+    </div>
+  </div>
+
+      <div class="form-group row">
+    <label for="prenom" class="col-sm-2 col-form-label">Prénom</label>
+    <div class="col-sm-4">
+      <input type="text" class="form-control" name="prenom" placeholder="Prénom">
+    </div>
+  </div>
+
+    <div class="form-group row">
+    <label for="categorie" class="col-sm-2 col-form-label">Catégorie</label>
+    <div class="col-sm-4">
+      <input type="text" class="form-control" name="categorie" placeholder="Catégorie">
+    </div>
+  </div>
+
+    <div class="form-group row">
+    <label for="ligue" class="col-sm-2 col-form-label">Ligue</label>
+    <div class="col-sm-4">
+      <input type="text" class="form-control" name="ligue" placeholder="Ligue">
+    </div>
+  </div>
+
+    <div class="form-group row">
+    <label for="club" class="col-sm-2 col-form-label">Club</label>
+    <div class="col-sm-4">
+      <input type="text" class="form-control" name="club" placeholder="Club">
+    </div>
+  </div>
+
+    <div class="form-group row">
+    <label for="Ville" class="col-sm-2 col-form-label">ville</label>
+    <div class="col-sm-4">
+      <input type="text" class="form-control" name="Ville" placeholder="ville">
+    </div>
+  </div>
+
+    <div class="form-group row">
+    <label for="fonction" class="col-sm-2 col-form-label">Fonction</label>
+    <div class="col-sm-4">
+      <input type="text" class="form-control" name="fonction" placeholder="Fonction">
+    </div>
+  </div>
+
 <!--
 No de licence: &nbsp   &nbsp &nbsp &nbsp &nbsp &nbsp   <input type="text" name="no_licence"><br>
 Type d'identité: &nbsp    &nbsp  <input type="text" name="type_d_identite"><br>
 ID identité: &nbsp    &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  <input type="text" name="id_identite"><br>
--->
 Nom: &nbsp     &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp <input type="text" name="nom"><br> 
 Prénom: &nbsp    &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  <input type="text" name="prenom"> <br>
 Catégorie: &nbsp      <input type="text" name="categorie"> <br>
@@ -124,9 +172,15 @@ Ligue: &nbsp      <input type="text" name="ligue"><br>
 Club: &nbsp      <input type="text" name="club"><br> 
 Ville: &nbsp      <input type="text" name="ville"><br>
 Fonction: &nbsp      <input type="text" name="fonction"><br>
-
-
 <input type="submit" value="Sauvegarder">
+
+-->
+<div class="form-group row">
+    <div class="col-sm-10">
+      <button type="submit" class="btn btn-lg btn-primary">Sauvegarder</button>
+    </div>
+  </div>
+
 
 </form>
 
