78656fe0f52bb75a309ed8ed25aa9d88fb790f11
brookkk <grini.abdellah@gmail.com>
2018-07-06 10:37:06 +0100

    create adherent htmlspecialchars

--- webapp/adherentBundle/Controllers/AdherentController.php
+++ webapp/adherentBundle/Controllers/AdherentController.php
@@ -4,26 +4,24 @@ session_start();
 include '../Classes/Crud_adherent.php';
 //include '../../fina-navigation.php';
 
-$no_licence = $_POST['no_licence'];
-$type_d_identite = $_POST['type_d_identite'];
+$no_licence = htmlspecialchars($_POST['no_licence']);
+$type_d_identite = htmlspecialchars($_POST['type_d_identite']);
 //echo (1/$new_var);
-$id_identite = $_POST['id_identite'];
-$nom = $_POST['nom'];
-$prenom = $_POST['prenom'];
-$categorie = $_POST['categorie'];
-$ligue = $_POST['ligue'];
-$club = $_POST['club'];
-$ville = $_POST['ville'];
-$fonction = $_POST['fonction'];
+$id_identite = htmlspecialchars($_POST['id_identite']);
+$nom = htmlspecialchars($_POST['nom']);
+$prenom = htmlspecialchars($_POST['prenom']);
+$categorie = htmlspecialchars($_POST['categorie']);
+$ligue = htmlspecialchars($_POST['ligue']);
+$club = htmlspecialchars($_POST['club']);
+$ville = htmlspecialchars($_POST['ville']);
+$fonction = htmlspecialchars($_POST['fonction']);
 
-//$no_licence = "hoho";
 $user = $_SESSION['pseudo'];
 //print_r($_SESSION['pseudo']);
 
 $adherent = new Crud_adherent();
 //if($adherent->ajouterAdherent($no_licence)){
 if($adherent->ajouterAdherent($no_licence, $type_d_identite, $id_identite, $nom, $prenom, $categorie, $ligue, $club, $ville, $fonction, $user)){
-	//echo "good";
 	header("Location: ../../adherents.php");
 }
 
